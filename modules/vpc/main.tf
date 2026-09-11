d49a9c8e0687d15bfd3e51a5476cfad04a12024e
Vikrant Chitte <vicky12chitte@gmail.com>
2026-09-11 19:40:50 +0530

    adding nat_gateway_id from private_rt

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -62,7 +62,7 @@ resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat.id
   }
   tags = merge(var.tags, { Name = "${var.environment_name}-private-rt" })
 }
